277d583174cb767890fed3b03c2c12185bbf6a11
Linex <26711215+linex-cd@users.noreply.github.com>
2022-10-15 06:30:11 +0800

    Update install.sh

--- deploy/install.sh
+++ deploy/install.sh
@@ -16,13 +16,27 @@ docker build -f ./Dockerfile -t ${image_name}:${version} .
 rm Dockerfile
 cd ./deploy
 
+#make dir for data
+if [ ! -d "/data" ]; then
+	echo 'Making path (/data) for cache...'
+	mkdir /data
+fi
 
+if [ ! -d "/data/fsa" ]; then
+	echo 'Making path (/data/fsa) for cache...'
+	mkdir /data/fsa
+fi
 
+if [ ! -d "/data/fsa/data" ]; then
+	echo 'Making path (/data/fsa/data) for cache...'
+	mkdir /data/fsa/data
+fi
 
 docker run \
 --name=${container_name} \
 --network=host \
 -e SERVER_PORT=${SERVER_PORT} \
+-v /data/fsa/data/:/data \
 --restart=always \
 -d ${image_name}:${version} \
 python3 main.py
